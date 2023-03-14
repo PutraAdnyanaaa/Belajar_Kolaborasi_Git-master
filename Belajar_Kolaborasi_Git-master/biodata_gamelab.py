b227b645e225aa9186d17eaae550ac98d53cbd3b
PutraAdnyanaaa <darmiasih121113@gmail.com>
2023-03-14 09:32:39 +0800

    first commit

---
+++ Belajar_Kolaborasi_Git-master/biodata_gamelab.py
@@ -0,0 +1,21 @@
+print ("=======================================")
+print ("Program Biodata Sederhana Dengan Python")
+print ("=======================================")
+ 
+nama = "I Gede Komang Putra Adnyana"
+hobi = "mengambar"
+alamat = "Jalan Majapahit G.permata asri no.9"
+Kota = "Tabanan"
+Propinsi = "Bali"
+
+print("Nama Lengkap  :",nama)
+
+print("Alamat        :",alamat, "\n                Kota ", Kota, "Propinsi ",Propinsi)
+
+
+print("Hobi          :", hobi)
+
+print ("Selamat Datang di",nama )
+print ("=======================================")
+ 
+
